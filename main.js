98102dbeea7b9b54fe58525099b2b32c03d00a95
arjunambekar-09 <82198977+arjunambekar-09@users.noreply.github.com>
2022-02-24 11:27:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+function iron_man_1(){
+    window.location = "iron_man_1.html";
+}
+function i(){
+    window.location = "index.html";
+}
+function hulk(){
+    window.location = "hulk.html";
+}
+function iron_man_2(){
+    window.location = "iron_man_2.html";
+}
+function thor_1(){
+    window.location = "thor_1.html";
+}
+function captain_america_1(){
+    window.location = "captain_america_1.html";
+}
+function avengers_1(){
+    window.location = "avengers_1.html";
+}
+function iron_man_3(){
+    window.location = "iron_man_3.html";
+}
+function thor_2(){
+    window.location = "thor_2.html";
+}
+function captain_america_2(){
+    window.location = "captain_america_2.html";
+}
+function guardians_of_the_galaxy_1(){
+    window.location = "guardians_of_the_galaxy_1.html";
+}
+function avengers_2(){
+    window.location = "avengers_2.html";
+}
+function ant_man(){
+    window.location = "ant-man.html";
+}
+function captain_america_3(){
+    window.location = "captain_america_3.html";
+}
+function doctor_strange(){
+    window.location = "doctor_strange.html";
+}
+function guardians_of_the_galaxy_2(){
+    window.location = "guardians_of_the_galaxy_2.html";
+}
+function spider_man_1(){
+    window.location = "spider_man_1.html";
+}
+function thor_3(){
+    window.location = "thor_3.html";
+}
+function black_panther(){
+    window.location = "black_panther.html";
+}
+function avengers_3(){
+    window.location = "avengers_3.html";
+}
+function ant_man_2(){
+    window.location = "ant_man_2.html";
+}
+function captain_marvel(){
+    window.location = "captain_marvel.html";
+}
+function avengers_4(){
+    window.location = "avengers_4.html";
+}
+function spider_man_2(){
+    window.location = "spider_man_2.html"
+}
